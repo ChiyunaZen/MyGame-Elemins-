bbc90421720602f4d9f283a026f3ff51defb8f6f
xknamx <tsunacan0101@gmail.com>
2024-11-04 04:40:58 +0900

    アニメーション

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,22 +7,19 @@ public class CharacterControllerCheckGround : MonoBehaviour
     private CharacterController controller;
     private Animator animator;
     private Vector3 velocity = Vector3.zero; //
-    [SerializeField]
-    private float moveSpeed = 1.5f;
-    [SerializeField]
-    private float jumpPower = 5f;
-    //　下方向に強制的に加える力
-    [SerializeField]
-    private Vector3 addForceDownPower = Vector3.down;
 
-    // Use this for initialization
+    [SerializeField]private float moveSpeed = 1.5f;
+    [SerializeField]private float jumpPower = 5f;
+    
+    //　下方向に強制的に加える力
+    [SerializeField] private Vector3 addForceDownPower = Vector3.down;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
