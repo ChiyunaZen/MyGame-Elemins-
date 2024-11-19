10dd1d914d7807c933fbf22f8839d8da8aaf4f12
xknamx <tsunacan0101@gmail.com>
2024-11-20 08:50:29 +0900

    testBuilds

--- Assets/Scripts/EleminController.cs
+++ Assets/Scripts/EleminController.cs
@@ -1,9 +1,4 @@
-using lilToon;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
