add86f862455480c45d5137359fff934575a2980
ChiyunaZen <uni202406_11@telework.to>
2024-12-02 13:32:31 +0900

    ダイアログの非アクティブが重複していたのを修正

--- Assets/Scripts/UI/UI_ExitDialog.cs
+++ Assets/Scripts/UI/UI_ExitDialog.cs
@@ -8,7 +8,7 @@ public class UI_ExitDialog : MonoBehaviour
 
     private void Start()
     {
-        exitDialog.SetActive(false);
+        //exitDialog.SetActive(false);
     }
 
     public void Show()
